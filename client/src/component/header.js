13769c738885653e284196bd7fe8dafc3b2d8cd5
kwonc <jabykw@gmail.com>
2021-12-31 17:18:15 +0900

    header setting

--- client/src/component/header.js
+++ client/src/component/header.js
@@ -1,7 +1,17 @@
 import React from "react"
+import {Row, Col} from "reactstrap";
 
 const Header = () => {
-    return <div>Header</div>;
+    return (
+        <div id="header1" className="mb-3">
+           <Row>
+               <Col md="6" sm="auto" className="text-center m-auto">
+                   <h1>header text 1</h1>
+                   <p>header text 2</p>
+               </Col>
+           </Row>
+        </div>
+    );
 };
 
 export default Header;
