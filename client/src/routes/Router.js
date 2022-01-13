cec4d6d9d81973e2dfb50e8c5671c51f751889d7
kwonc <jabykw@gmail.com>
2022-01-13 22:44:00 +0900

    Create various routers

--- client/src/routes/Router.js
+++ client/src/routes/Router.js
@@ -2,12 +2,28 @@ import React, {Fragment} from "react";
 import Header from "../component/header";
 import Footer from "../component/footer";
 import AppNavbar from "../component/AppNavbar";
+import {Container} from "reactstrap";
+import {Redirect, Route, Switch} from "react-router-dom";
+import PostCardList from "./route/PostCardList";
+import PostWrite from "./route/PostWrite";
+import PostDetail from "./route/PostDetail";
+import CategoryResult from "./route/CategoryResult";
+import Search from "./route/Search";
 
 const MyRouter = () => (
     <Fragment>
         <AppNavbar/>
         <Header/>
-        <h1>Body</h1>
+        <Container id="main-body">
+            <Switch>
+                <Route path="/" exact component={PostCardList}/>
+                <Route path="/post" exact component={PostWrite}/>
+                <Route path="/post/:id" exact component={PostDetail}/>
+                <Route path="/post/:category/:categoryName" exact component={CategoryResult}/>
+                <Route path="/search/:searchTerm" exact component={Search}/>
+                <Redirect from="*" to="/" />
+            </Switch>
+        </Container>
         <Footer/>
     </Fragment>
 )
